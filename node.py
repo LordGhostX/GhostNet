eae12e9fb0232e5c80f867473f4cabcfea623371
LordGhostX <solomonghost9@gmail.com>
2023-12-28 13:33:43 +0100

    implement node exchange

--- node.py
+++ node.py
@@ -9,9 +9,13 @@ from flask import *
 app = Flask(__name__)
 CONNECTED_NODES = dict()
 RELAYED_MESSAGES = dict()
+
+# constants/enums
 PROTOCOL = "http://"
 DEFAULT_PORT = 6969
 NODE_ATTEMPTS = 3
+RELAY_MESH = 5
+MAX_PX_NODES = 5
 
 # returns the node address for other nodes to connect to
 def get_node_address() -> str:
@@ -27,11 +31,11 @@ def ping_node() -> str:
 
 
 # bootstraps the node with a list of external nodes
-# returns the list of node pings that were unsuccessful
+# returns the list of node pings that were successful and unsuccessful
 @app.route("/bootstrap", methods=["POST"])
-def bootstrap_node() -> list:
+def bootstrap_node() -> list[list, list]:
     nodes = request.get_json()["nodes"]
-    failed_node_pings = list()
+    successful_node_pings, failed_node_pings = list(), list()
     for node in nodes:
         # if the ping to external node is successful, add it to the list of connected nodes
         try:
@@ -40,15 +44,16 @@ def bootstrap_node() -> list:
             })
             if r.status_code == 200:
                 CONNECTED_NODES[node] = NODE_ATTEMPTS
+                successful_node_pings.append(node)
             else:
                 failed_node_pings.append(node)
         except:
             failed_node_pings.append(node)
             
-    return failed_node_pings
+    return [successful_node_pings, failed_node_pings]
 
 
-# returns the list of connected peers
+# returns the list of connected nodes
 @app.route("/nodes")
 def get_connected_nodes() -> list:
     return list(CONNECTED_NODES)
@@ -65,7 +70,7 @@ def relay_message() -> list:
         # store the hash of the message so the node doesn't relay more than once
         RELAYED_MESSAGES[message_hash] = 1
         # pick a random set of nodes to relay the message to
-        nodes = random.sample(list(CONNECTED_NODES), min(len(CONNECTED_NODES), 10))
+        nodes = random.sample(list(CONNECTED_NODES), min(len(CONNECTED_NODES), RELAY_MESH))
         successful_node_relays = list()
         for node in nodes:
             is_relay_unsuccessful = False
@@ -87,6 +92,38 @@ def relay_message() -> list:
                     del CONNECTED_NODES[node]
         print(message)
         return successful_node_relays
+
+
+# requests for new nodes to connect to from already connected ones
+# returns the new node connections created
+@app.route("/node-exchange")
+def node_exchange() -> list:
+    successful_node_pings, unsuccessful_node_pings = set(), set()
+    for node in list(CONNECTED_NODES):
+        # fetch all the nodes connected to an external node
+        r = requests.get(node + url_for("get_connected_nodes"))
+        if r.status_code == 200:
+            node_connections = random.sample(r.json(), min(len(r.json()), MAX_PX_NODES))
+        else:
+            node_connections = list()
+
+        # attempt to connect to each of the retrieved nodes
+        for connection in node_connections:
+            # if the node has connected before/attempted to connect/is the same node, skip the ping
+            if connection in set(CONNECTED_NODES) | successful_node_pings | unsuccessful_node_pings | {get_node_address()}:
+                continue
+            try:
+                r = requests.post(connection + url_for("ping_node"), json={
+                    "node": get_node_address()
+                })
+                if r.status_code == 200:
+                    CONNECTED_NODES[connection] = NODE_ATTEMPTS
+                    successful_node_pings.add(connection)
+                else:
+                    unsuccessful_node_pings.add(connection)
+            except:
+                unsuccessful_node_pings.add(connection)
+    return list(successful_node_pings)
 
 
 if __name__ == "__main__":
